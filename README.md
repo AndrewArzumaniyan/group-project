d36eee58fb4ac6a9d4f328c87772f5f1c23200ae
AndrewAnrzumaniyan <imsobaka@mail.ru>
2023-05-23 20:33:07 +0600

    asd

--- README.md
+++ README.md
@@ -1,13 +1,1 @@
-# Our first project
-
-## Our team
-1. Name 1 -
-2. Name 2 -
-3. Name 3 -
-
-[link to our site](https:://google.com)
-
-**OUR GOALS**
-* create design
-* create website
-* pulish website
+# Meme-memory-portfolio
--- js/main.js
+++ js/main.js
@@ -1,1 +1,51 @@
-console.log('It is main.js');
+let allCards = document.querySelectorAll('.memory-card');
+
+let startBtn = document.querySelector('.start-btn');
+
+startBtn.addEventListener('click', function () {
+  document.querySelector('.second').style.display = 'flex';
+  document.querySelector('.first').style.display = 'none';
+});
+
+let activeCards = [];
+let guessedCards = [];
+
+for (let i = 0; i < allCards.length; i++) {
+  allCards[i].addEventListener('click', function (e) {
+    if (guessedCards.includes(allCards[i]) || activeCards.includes(allCards[i])) {
+      return
+    }
+    
+    allCards[i].classList.add('active')
+    activeCards.push(allCards[i])
+    
+    if (activeCards.length === 2) {
+      if (activeCards[0].textContent === activeCards[1].textContent) {
+        guessedCards.push(activeCards[0])
+        guessedCards.push(activeCards[1])
+        activeCards = []
+      }
+    }
+    
+    if (activeCards.length === 3) {
+      if (activeCards[0].textContent === activeCards[1].textContent) {
+        guessedCards.push(activeCards[0])
+        guessedCards.push(activeCards[1])
+        activeCards = activeCards.slice(2)
+        
+        
+      } else {
+        activeCards[0].classList.remove('active')
+        activeCards[1].classList.remove('active')
+        activeCards = activeCards.slice(2)
+      }
+    }
+    
+    if (guessedCards.length === 16) {
+      console.log('победа')
+      document.querySelector('.final').style.display = 'flex';
+      document.querySelector('.second').style.display = 'none';
+      // переход на третий экран
+    }
+  });
+}
